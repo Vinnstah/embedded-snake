08a1aecbfd3b0c75a5fe901a8dce60ed3890106b
Viktor Jansson <v.l.jansson@gmail.com>
2023-12-28 19:25:17 +0100

    Working controls and refactor

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,25 @@
 #![no_main]
 #![no_std]
+mod snake;
 
 // #[macro_use]
 extern crate alloc;
 
-use alloc::vec::{Vec, self};
 use cortex_m_rt::entry;
-use rtt_target::rtt_init_print;
-use panic_rtt_target as _;
-// use embedded_alloc::LlffHeap as Heap;
 use embedded_alloc::Heap;
-use core::panic::PanicInfo;
 use embedded_hal as _;
 use microbit::{
-    board::Board,
-    display::blocking::Display,
-    hal::Timer,
-    hal::prelude::*
+    board::Board, board::Buttons, display::blocking::Display, hal::prelude::*, hal::Timer,
 };
-// Two-Level Segregated Fit Heap allocator (feature = "tlsf")
-// use embedded_alloc::TlsfHeap as Heap;
+use panic_rtt_target as _;
+use rtt_target::rtt_init_print;
+use snake::{Direction, Snake};
 
 #[global_allocator]
 static HEAP: Heap = Heap::empty();
 
 #[entry]
 fn main() -> ! {
-    
     {
         use core::mem::MaybeUninit;
         const HEAP_SIZE: usize = 1024;
@@ -39,86 +32,37 @@ fn main() -> ! {
 
     let mut timer = Timer::new(board.TIMER0);
 
-
-    let new_board = [[0, 0, 1, 0, 0],
-    [0, 0, 1, 0, 0],
-    [1, 0, 1, 0, 1],
-    [0, 0, 1, 0, 0],
-    [0, 0, 1, 0, 0]
-    ];
-
-    let mut default_board = [[0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0]
+    let mut default_board = [
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
     ];
 
     let mut display = Display::new(board.display_pins);
     let mut snake = Snake::new();
 
     loop {
+        if let Ok(true) = board.buttons.button_b.is_low() {
+            match snake.current_direction {
+                Direction::Down => snake._move(Direction::Right),
+                Direction::Right => snake._move(Direction::Down),
+                Direction::Left => snake._move(Direction::Down),
+                Direction::Up => snake._move(Direction::Right),
+            }
+        }
+        if let Ok(true) = board.buttons.button_a.is_low() {
+            match snake.current_direction {
+                Direction::Down => snake._move(Direction::Left),
+                Direction::Right => snake._move(Direction::Up),
+                Direction::Left => snake._move(Direction::Up),
+                Direction::Up => snake._move(Direction::Left),
+            }
+        }
         for snake_bits in &snake.body_position {
             default_board[snake_bits.0][snake_bits.1] = 1;
-            display.show(&mut timer, default_board, 100)
+            display.show(&mut timer, default_board, 100);
         }
-        if let Ok(true) = board.buttons.button_b.is_high() {
-            snake._move(Direction::Down)
-        }
-
-        // display.show(&mut timer, new_board, 300);
-        // display.show(&mut timer, default_board, 300);
-        // row1.set_low().unwrap();
-        // rprintln!("Dark!");
-        // row1.set_high().unwrap();
-        // rprintln!("Light!");
-    }
-}
-
-// #[panic_handler]
-// fn panic(_: &PanicInfo) -> ! {
-//     loop {}
-// }
-
-
-enum Direction {
-    Down,
-    Right,
-}
-
-struct Snake {
-    body_length: u8,
-    body_position: Vec<(usize, usize)>,
-}
-
-impl Snake {
-    fn _move(&mut self, move_direction: Direction) {
-        let current_head = self.body_position.first().unwrap();
-        let mut new_head = (0, 0);
-        match move_direction {
-            Direction::Down =>  { 
-                if current_head.1 == 4 { 
-                    new_head.1 = 0;
-                    self.body_position.insert(0, new_head)
-                } else {
-                    if current_head.1 == 0 {
-                        new_head.1 = current_head.1 - 1;
-                        self.body_position.insert(0, new_head)  
-                    } 
-                    self.body_position.insert(0, new_head)  
-                }
-            },
-            Direction::Right => { 
-
-                new_head.1 = current_head.1 + 1
-
-            } 
-        }
-    }
-
-    fn new() -> Self {
-        let mut body_pos: Vec<(usize, usize)> = Vec::new();
-        body_pos = [(2, 2), (2, 3)].to_vec();
-        Self { body_length: 2, body_position: body_pos.to_vec() }
     }
 }
---
+++ src/snake.rs
@@ -0,0 +1,135 @@
+#![no_main]
+#![no_std]
+use alloc::vec::{self, Vec};
+use microbit::board::Buttons;
+
+#[derive(PartialEq, Copy, Clone)]
+pub enum Direction {
+    Down,
+    Right,
+    Left,
+    Up,
+}
+
+pub struct Snake {
+    body_length: u8,
+    pub body_position: Vec<(usize, usize)>,
+    pub current_direction: Direction,
+}
+
+impl Snake {
+    pub fn _move(&mut self, move_direction: Direction) {
+        let mut current_head: &(usize, usize) = self.body_position.first().unwrap();
+        match move_direction {
+            Direction::Down => {
+                self.body_position
+                    .insert(0, Snake::move_down(*current_head));
+                self.current_direction = Direction::Down
+            }
+            Direction::Right => {
+                self.body_position
+                    .insert(0, Snake::move_right(*current_head));
+                self.current_direction = Direction::Right
+            }
+            Direction::Left => {
+                self.body_position
+                    .insert(0, Snake::move_left(*current_head));
+                self.current_direction = Direction::Left
+            }
+            Direction::Up => {
+                self.body_position.insert(0, Snake::move_up(*current_head));
+                self.current_direction = Direction::Up
+            }
+        }
+    }
+
+    fn determine_direction(&self, input: Buttons) -> Direction {
+        let current_direction: Direction = self.current_direction;
+        match input {
+            button_a => {
+                if current_direction == Direction::Left {
+                    Direction::Down
+                } else if current_direction == Direction::Up || current_direction == Direction::Down
+                {
+                    Direction::Left
+                } else {
+                    panic!()
+                }
+            }
+            button_b => {
+                if current_direction == Direction::Right {
+                    Direction::Up
+                } else if current_direction == Direction::Up || current_direction == Direction::Down
+                {
+                    Direction::Right
+                } else {
+                    panic!()
+                }
+            }
+        }
+    }
+
+    pub fn new() -> Self {
+        let mut body_pos: Vec<(usize, usize)> = Vec::new();
+        body_pos = [(0, 0)].to_vec();
+        Self {
+            body_length: 1,
+            body_position: body_pos,
+            current_direction: Direction::Right,
+        }
+    }
+}
+
+impl Snake {
+    fn move_down(current_head: (usize, usize)) -> (usize, usize) {
+        let mut new_head: (usize, usize) = (0, 0);
+        if current_head.0 == 4 {
+            new_head.0 = 0;
+            new_head.1 = current_head.1;
+            new_head
+        } else {
+            new_head.0 = current_head.0 + 1;
+            new_head.1 = current_head.1;
+            new_head
+        }
+    }
+
+    fn move_right(current_head: (usize, usize)) -> (usize, usize) {
+        let mut new_head: (usize, usize) = (0, 0);
+        if current_head.1 == 4 {
+            new_head.1 = 0;
+            new_head.0 = current_head.0;
+            new_head
+        } else {
+            new_head.1 = current_head.1 + 1;
+            new_head.0 = current_head.0;
+            new_head
+        }
+    }
+
+    fn move_left(current_head: (usize, usize)) -> (usize, usize) {
+        let mut new_head: (usize, usize) = (0, 0);
+        if current_head.1 == 0 {
+            new_head.1 = 4;
+            new_head.0 = current_head.0;
+            new_head
+        } else {
+            new_head.1 = current_head.1 - 1;
+            new_head.0 = current_head.0;
+            new_head
+        }
+    }
+
+    fn move_up(current_head: (usize, usize)) -> (usize, usize) {
+        let mut new_head: (usize, usize) = (0, 0);
+        if current_head.0 == 0 {
+            new_head.0 = 4;
+            new_head.1 = current_head.1;
+            new_head
+        } else {
+            new_head.0 = current_head.0 - 1;
+            new_head.1 = current_head.1;
+            new_head
+        }
+    }
+}
